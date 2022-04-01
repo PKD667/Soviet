84cda2c9172ec17f2d006bd7fd2fcf29eafd1d4e
JohnVictoryz <85512286+JohnVictoryz@users.noreply.github.com>
2022-04-01 18:37:48 +0300

    Update main.cpp
    Removed std:: and replaced with a namespace for the code to be easier for the PC to run and the eye to relax

--- CCCP/src/main.cpp
+++ CCCP/src/main.cpp
@@ -2,25 +2,25 @@
 #include <fstream>
 #include <string>
 #include <vector>
+using namespace std;
 
-
-std::string CURRENT_DIR = "/home/paulk/Desktop/Soviet/CCCP/";
-std::string BUILD_ROOT = CURRENT_DIR + "build-root/";
+string CURRENT_DIR = "/home/paulk/Desktop/Soviet/CCCP/";
+string BUILD_ROOT = CURRENT_DIR + "build-root/";
 
 //Getting package data from file
-std::vector<std::string> open_spm (std::string PName)
+vector<string> open_spm (string PName)
 {
-    std::streampos size;
+    streampos size;
     char * memblock;
 
-    std::string PPath = CURRENT_DIR + "testing/" + PName + "/";
-    std::cout << PPath + PName + ".spm \n";
-    std::ifstream file_spm (PPath + PName + ".spm", std::ios::in);
-    std::string line;
-    std::vector<std::string> pkg_info;
+    string PPath = CURRENT_DIR + "testing/" + PName + "/";
+    cout << PPath + PName + ".spm \n";
+    ifstream file_spm (PPath + PName + ".spm", ios::in);
+    string line;
+    vector<string> pkg_info;
     if (file_spm.is_open())
     {
-        std::cout << "file opened " << "\n";
+        cout << "file opened " << "\n";
         while ( getline (file_spm,line) )
         {
         pkg_info.push_back(line);
@@ -30,8 +30,8 @@ std::vector<std::string> open_spm (std::string PName)
     }
 
     else {
-        std::cout << "Unable to open file \n";
-        return std::vector<std::string> () ;
+        cout << "Unable to open file \n";
+        return vector<string> () ;
     }
 
     
@@ -42,25 +42,25 @@ int install_binary ()
 
 }
 //parsing data and installing package
-int install_package (std::string PName)
+int install_package (string PName)
 {
-    std::cout << "processing package" << "\n";
-    std::vector<std::string> pkg_info = open_spm("lolcat");
-    std::cout << "package info parsed" << "\n";
+    cout << "processing package" << "\n";
+    vector<string> pkg_info = open_spm("lolcat");
+    cout << "package info parsed" << "\n";
     if ( pkg_info[0] != "nothing")
     {
     //Dependencies stuff for LATER
     }
     else {
-        std::cout << "No dependencies" << "\n";
+        cout << "No dependencies" << "\n";
         if ( pkg_info[1] == "source") 
         {
-            std::cout << "building from source" << "\n";
+            cout << "building from source" << "\n";
             if (pkg_info[2] == "make")
             {
-                std::cout << "Making pkg" << "\n";
-                std::string cmd_make = "( cd " + CURRENT_DIR + "testing/" + PName + "/" + PName + " && make && make DESTDIR=" + BUILD_ROOT + " install )  ";
-                std::cout << cmd_make << std::endl;
+                cout << "Making pkg" << "\n";
+                string cmd_make = "( cd " + CURRENT_DIR + "testing/" + PName + "/" + PName + " && make && make DESTDIR=" + BUILD_ROOT + " install )  ";
+                cout << cmd_make << endl;
                 system(cmd_make.c_str());
             }
         }
